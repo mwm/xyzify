fc8079fdc31ccc8993d135f647376e491b905af2
Mike Meyer <mike@fpcomplete.com>
2014-05-19 05:17:07 +0000

    Weird. Compilation broken when I tried to deploy it. Fixed that now as well. Plus code cleanup

--- src/Test.hs
+++ src/Test.hs
@@ -5,7 +5,6 @@ module Test where
 import Prelude hiding (putStrLn, readFile)
 import Data.ByteString.Lazy.Char8 (readFile, pack, putStrLn, ByteString)
 import Data.ByteString.Base64.Lazy (decode)
-import Data.Either.Combinators (fromRight)
 import System.IO (hPutStrLn, stderr)
 
 import GCode
