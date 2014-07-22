ba85d78c4857a87408cdb1056a5c14565d85a191
Mike Meyer <mwm@mired.org>
2014-07-22 12:43:22 -0500

    Fix main to handle arguments and deal with errors better.

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,37 @@
+#!/usr/bin/env runhaskell
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Prelude hiding (getContents, putStrLn)
-import Data.ByteString.Lazy.Char8 (getContents, putStrLn)
-import System.IO (hPutStrLn, stderr)
 
-import GCode
+import Data.ByteString.Lazy.Char8 (hGetContents, hPutStrLn)
+import Control.Error (runScript, scriptIO, tryIO, throwT, tryRight)
+import Control.Monad (liftM2)
+import System.Environment (getArgs, getProgName)
+import System.Exit (exitWith, ExitCode(ExitSuccess))
+import System.FilePath (FilePath, takeExtension, addExtension, replaceExtension)
+import System.IO (IOMode(ReadMode, WriteMode), openFile, hClose,
+                  stdin, stdout, stderr)
+
+import GCode (makeXYZ)
 
 main :: IO ()
-main = getContents >>= either (hPutStrLn stderr) putStrLn . makeXYZ
+main = runScript $ do
+  myName <- scriptIO getProgName
+  as <- scriptIO getArgs
+  (hIn, hOut) <-
+        case as of []  -> return (stdin, stdout)
+                   [f] -> do hI <- scriptIO $ openFile f ReadMode
+                             hO <- scriptIO $ openFile (fixName f) WriteMode
+                             return (hI, hO)
+                   _   -> throwT $ "Usage: " ++ myName ++ " [inputfile]"
+  dIn <- scriptIO $ hGetContents hIn
+  res <- tryRight $ makeXYZ dIn
+  scriptIO $ hPutStrLn hOut res
+  mapM_ (scriptIO . hClose) [hIn, hOut]
+
+fixName :: FilePath -> FilePath
+fixName nIn =
+  (if takeExtension nIn == ".gcode" then replaceExtension else addExtension)
+  nIn ".3w"
