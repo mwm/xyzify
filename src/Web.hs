460981e468b66cec8bb7f2ffa598506718b6c6fe
Mike Meyer <mwm@mired.org>
2014-07-25 16:53:49 -0500

    Add a file download option.

--- src/Web.hs
+++ src/Web.hs
@@ -6,7 +6,9 @@ module Web where
 import Prelude hiding (span)
 
 import Control.Error (runEitherT, scriptIO, tryRight)
-import Data.ByteString.Lazy.Char8 (hGetContents)
+import Data.ByteString.Lazy.Char8 (hGetContents, append)
+import Data.ByteString.Char8 (pack)
+import MFlow.Forms.Internals (getToken)
 import MFlow.Wai.Blaze.Html.All hiding (id)
 import System.IO (IOMode(ReadMode), openFile)
 
@@ -18,6 +20,7 @@ main :: IO ()
 main = runNavigation "" . step . page $ do
   setFilesPath "./"
   file <- h1 "XYZifier"
+          ++> script "" ! src "dropzone.js"
           ++> p << a "Need help?" ! href "help.html"
           ++> fileUpload <** br ++> submitButton "Convert"
   process file
@@ -28,9 +31,19 @@ process (name, _, input) = do
     dIn <- scriptIO $ openFile input ReadMode >>= hGetContents
     res <- tryRight $ makeXYZ dIn
     let out = fixName name
-    -- Code to download ByteString res to file name goes here!
+    tok <- getToken
+    scriptIO . sendFlush tok . HttpData [("Content-Type", "text/plain"),
+                                         ("Cache-Control", "max-age=360000"),
+                                         ("Content-Disposition",
+                                          pack $ "attachment; filename=" ++ out)]
+                                        [] $ res `append` "\n"
     return $ out
   either (format toHtml $ b "Conversion failed: ") (format code "Downloading ") res
 
 format :: (Html -> Html) -> Html -> String -> View Html IO ()
 format tag pre msg = p (toHtml pre >> tag << toHtml msg) ++> noWidget
+
+rawSend :: (FormInput v,MonadIO m) => HttpData -> View v m ()
+rawSend dat =  do
+    tok <- getToken
+    liftIO $ sendFlush tok dat
